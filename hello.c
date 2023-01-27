c1d8805a53b9b2af44e7d32157b1bf54a2ff6365
jwoodard839 <123711311+jwoodard839@users.noreply.github.com>
2023-01-27 11:46:24 -0600

    added my name and major to the output

--- hello.c
+++ hello.c
@@ -12,6 +12,8 @@ int main(int argc, char **argv) {
 
   printf("Joe Brewer!\n");
   printf("My major is electrical engineering\n");
+  printf("Jeff Woodard\n");
+  printf("My major is electrical engineering\n");
 
   return 0;
 }
