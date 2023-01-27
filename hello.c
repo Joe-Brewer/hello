8c1978d5b5b88d6bc6919e3606afca27cbb2b560
Joe Brewer <joebrewer@unomaha.edu>
2023-01-27 16:25:18 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Joe Brewer
+ * Date: 2023/01/27
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Joe Brewer!\n");
+
+  return 0;
+}
